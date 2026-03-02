2ca4bb3b3e3d868d16c60eea1f82f3f2b6519f71
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-03-02 18:31:41 -0300

    Commit

---
+++ src/Iniciante1004.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Iniciante1003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int v1 = sc.nextInt(), v2 = sc.nextInt();
+
+        System.out.printf("PROD = %d%n", v1 * v2);
+    }
+}
