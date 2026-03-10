c4a4975afb3160706e608106575e66b3ead5506a
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-03-10 16:08:40 -0300

    feat: solução dos problemas 1040 a 1042 do beecrowd

---
+++ src/Iniciante1040.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Iniciante1040 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double n1 = sc.nextDouble(), n2 = sc.nextDouble(), n3 = sc.nextDouble(), n4 = sc.nextDouble();
+        double media = (n1 * 2.0 + n2 * 3.0 + n3 * 4.0 + n4 * 1.0) / 10.0;
+
+        System.out.printf("Media: %.1f%n", media - 0.00001);
+
+        if (media >= 7.0) {
+            System.out.println("Aluno aprovado.");
+        } else if (media < 5.0) {
+            System.out.println("Aluno reprovado.");
+        } else {
+            System.out.println("Aluno em exame.");
+
+            double notaDoExame = sc.nextDouble();
+            double mediaFinal = (notaDoExame + media) / 2;
+
+            System.out.printf("Nota do exame: %.1f%n", notaDoExame);
+
+            if (mediaFinal >= 5.0) {
+                System.out.println("Aluno aprovado.");
+            } else {
+                System.out.println("Aluno reprovado.");
+            }
+
+            System.out.printf("Media final: %.1f%n", mediaFinal);
+        }
+    }
+}
---
+++ src/Iniciante1042.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Iniciante1042 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int valor1 = sc.nextInt(), valor2 = sc.nextInt(), valor3 = sc.nextInt();
+        int[] valores = {valor1, valor2, valor3};
+        int[] valoresCrescente = {valor1, valor2, valor3};
+        Arrays.sort(valoresCrescente);
+
+        for (int valor : valoresCrescente) {
+            System.out.println(valor);
+        }
+
+        System.out.println();
+
+        for (int valor : valores) {
+            System.out.println(valor);
+        }
+    }
+}
