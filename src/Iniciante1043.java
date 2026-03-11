abf483b233dd3f4e89928abb11e3193e91e69a5b
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-03-11 18:16:28 -0300

    feat: solução dos problemas 1043 e 1044 do beecrowd

---
+++ src/Iniciante1043.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Iniciante1043 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble(), b = sc.nextDouble(), c = sc.nextDouble();
+
+        if ((b + c) > a && (a + c) > b && (a + b) > c) {
+            double perimetro = a + b + c;
+            System.out.printf("Perimetro = %.1f%n", perimetro);
+        } else {
+            double area = (a + b) * c / 2;
+            System.out.printf("Area = %.1f%n", area);
+        }
+    }
+}
