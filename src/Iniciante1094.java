278ab2dca10040ead753216cd21f818ff33d4c8f
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-03-18 15:42:30 -0300

    feat: solução dos problemas 1094 a 1115 do beecrowd

---
+++ src/Iniciante1094.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Iniciante1094 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(), total = 0, totalC = 0, totalR = 0, totalS = 0;
+
+        for (int i = 0; i < n; i++) {
+            int quantia = sc.nextInt();
+            char tipo = sc.next().charAt(0);
+            total += quantia;
+
+            switch (tipo) {
+                case 'C':
+                    totalC += quantia;
+                    break;
+                case 'R':
+                    totalR += quantia;
+                    break;
+                case 'S':
+                    totalS += quantia;
+                    break;
+            }
+        }
+
+        double percentualC = totalC * 100.0 / total;
+        double percentualR = totalR * 100.0 / total;
+        double percentualS = totalS * 100.0 / total;
+
+        System.out.println(
+                "Total: " + total + " cobaias\n" +
+                "Total de coelhos: " + totalC + "\n" +
+                "Total de ratos: " + totalR + "\n" +
+                "Total de sapos: " + totalS + "\n" +
+                String.format("Percentual de coelhos: %.2f ", percentualC) + "%\n" +
+                String.format("Percentual de ratos: %.2f ", percentualR) + "%\n" +
+                String.format("Percentual de sapos: %.2f ", percentualS) + "%"
+        );
+    }
+}
---
+++ src/Iniciante1099.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Iniciante1099 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt(), y = sc.nextInt();
+            int menor = Math.min(x, y), maior = Math.max(x, y), soma = 0;
+
+            for (int j = menor + 1; j < maior; j++) {
+                if (j % 2 != 0) {
+                    soma += j;
+                }
+            }
+
+            System.out.println(soma);
+        }
+    }
+}
