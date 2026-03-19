b8adc39ad65f6ad7a895e47776366505fa4c79bd
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-03-19 06:57:49 -0300

    feat: solução dos problemas 1144 a 1146 do beecrowd

---
+++ src/Iniciante1144.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Iniciante1144 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(), x = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(
+                    x + " " + (x * x) + " " + (x * x * x) + "\n" +
+                    x + " " + (x * x + 1) + " " + (x * x * x + 1)
+            );
+
+            x++;
+        }
+    }
+}
---
+++ src/Iniciante1146.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Iniciante1146 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int x = sc.nextInt();
+
+            if (x == 0) {
+                break;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i <= x; i++) {
+                sb.append(i);
+                if (i < x) sb.append(" ");
+            }
+
+            System.out.println(sb);
+        }
+    }
+}
