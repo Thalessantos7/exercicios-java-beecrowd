0847e1d4204c412f46c6a70cf30c083f2b94feb8
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-03-19 10:09:38 -0300

    feat: solução dos problemas 1149 a 1157 do beecrowd

---
+++ src/Iniciante1154.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Iniciante1154 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int soma, cont;
+        soma = cont = 0;
+
+        while (true) {
+            int idade = sc.nextInt();
+
+            if (idade < 0) {
+                break;
+            }
+
+            soma += idade;
+            cont++;
+        }
+
+        System.out.printf("%.2f%n", (double) soma / cont);
+    }
+}
