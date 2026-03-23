5c3ebd0e9841f274cf546f200893354f31a736a6
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-03-23 14:43:36 -0300

    feat: solução dos problemas 1165 a 1174 do beecrowd

---
+++ src/Iniciante1165.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Iniciante1165 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt(), cont = 0;
+
+            for (int j = 1; j <= num * 2; j++) {
+                if (num % j == 0) {
+                    cont++;
+                }
+            }
+
+            if (cont == 2) {
+                System.out.println(num + " eh primo");
+            } else {
+                System.out.println(num + " nao eh primo");
+            }
+        }
+    }
+}
