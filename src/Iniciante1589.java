431a765f952344f67a87a5367913b94c16f8ba1d
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-04-08 15:42:09 -0300

    feat: solução dos problemas 1564 a 1759 do beecrowd

---
+++ src/Iniciante1589.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Iniciante1589 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int r1 = sc.nextInt(), r2 = sc.nextInt();
+
+            System.out.println(r1 + r2);
+        }
+    }
+}
---
+++ src/Iniciante1759.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Iniciante1759 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n - 1; i++) {
+            System.out.print("Ho ");
+        }
+
+        System.out.println("Ho!");
+    }
+}
