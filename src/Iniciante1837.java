62fbacaa1e606967c2c1715674f490d0d945e0b6
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-04-12 16:10:43 -0300

    feat: solução do problema 1837 do beecrowd

---
+++ src/Iniciante1837.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Iniciante1837 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        int q = a / b;
+        int r = a % b;
+
+        if (r < 0) {
+            r += Math.abs(b);
+
+            if (b > 0) {
+                q--;
+            } else {
+                q++;
+            }
+        }
+
+        System.out.println(q + " " + r);
+    }
+}
