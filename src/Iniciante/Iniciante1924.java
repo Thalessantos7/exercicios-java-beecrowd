a84b7e49649d0f134ac2bdcb75ac58795a1407bb
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-04-20 09:10:13 -0300

    feat: solução do problema 1924 do beecrowd

---
+++ src/Iniciante/Iniciante1924.java
@@ -0,0 +1,17 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+public class Iniciante1924 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String nomeDoCurso = sc.nextLine();
+        }
+
+        System.out.println("Ciencia da Computacao");
+    }
+}
