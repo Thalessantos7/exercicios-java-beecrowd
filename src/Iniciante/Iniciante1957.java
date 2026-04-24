ff7e9aae5e41c879a2829446c8a4db78e1b7bd91
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-04-24 12:01:55 -0300

    feat: solução do problema 1957 do beecrowd

---
+++ src/Iniciante/Iniciante1957.java
@@ -0,0 +1,13 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+public class Iniciante1957 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int v = sc.nextInt();
+
+        System.out.println(Integer.toHexString(v).toUpperCase());
+    }
+}
