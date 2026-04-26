2e56d41a183260e09518e72bbac3eee7b99bd12c
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-04-26 09:34:42 -0300

    feat: solução do problema 1959 do beecrowd

---
+++ src/Iniciante/Iniciante1959.java
@@ -0,0 +1,15 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+public class Iniciante1959 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextInt(), l = sc.nextInt();
+
+        System.out.println(n * l);
+
+        sc.close();
+    }
+}
