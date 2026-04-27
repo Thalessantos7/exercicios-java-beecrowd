212e4a53c538f9c983dbd0039b2ec55ef38555ff
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-04-27 12:31:24 -0300

    feat: solução do problema 1960 do beecrowd

---
+++ src/Iniciante/Iniciante1960.java
@@ -0,0 +1,25 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+public class Iniciante1960 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] valores = {900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanos = {"CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder resultado = new StringBuilder();
+
+        for (int i = 0; i < valores.length; i++) {
+            while (n >= valores[i]) {
+                resultado.append(romanos[i]);
+                n -= valores[i];
+            }
+        }
+
+        System.out.println(resultado.toString());
+        sc.close();
+    }
+}
