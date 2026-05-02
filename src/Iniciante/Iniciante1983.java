1ed7a7ac27f231c8f56a91e2d910ffb6e42b6fd9
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-05-02 15:46:30 -0300

    feat: solução do problema 1983 do beecrowd

---
+++ src/Iniciante/Iniciante1983.java
@@ -0,0 +1,43 @@
+package Iniciante;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Iniciante1983 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        int quantidadeDeAlunos = sc.nextInt();
+        double maiorPontuacao = 0;
+        long matriculaDoMaior = 0;
+
+        for (int i = 0; i < quantidadeDeAlunos; i++) {
+
+            long numeroDaMatricula = sc.nextLong();
+            double nota = sc.nextDouble();
+
+            if (nota > maiorPontuacao) {
+
+                maiorPontuacao = nota;
+                matriculaDoMaior = numeroDaMatricula;
+
+            }
+
+        }
+
+        if (maiorPontuacao >= 8) {
+
+            System.out.println(matriculaDoMaior);
+
+        } else {
+
+            System.out.println("Minimum note not reached");
+
+        }
+
+    }
+
+}
