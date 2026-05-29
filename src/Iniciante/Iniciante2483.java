4ce5ddc30bc93f756b45a81c3ec63504c8f5bce4
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-05-29 17:03:20 -0300

    feat: solução do problema 2483 do beecrowd

---
+++ src/Iniciante/Iniciante2483.java
@@ -0,0 +1,27 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+public class Iniciante2483 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int i = sc.nextInt();
+
+        System.out.print("Feliz nat");
+
+        for (int j = 0; j < i; j++) {
+
+            System.out.print("a");
+
+        }
+
+        System.out.println("l!");
+
+        sc.close();
+
+    }
+
+}
