d5025216bc33f670552e282745739a6a82ee508e
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-06-03 13:47:37 -0300

    feat: solução do problema 2510 do beecrowd

---
+++ src/Iniciante/Iniciante2510.java
@@ -0,0 +1,26 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+public class Iniciante2510 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < T; i++) {
+
+            String N = sc.nextLine();
+
+            System.out.println("Y");
+
+        }
+
+        sc.close();
+
+    }
+
+}
