87a73a71d6fec16c34282617676b0e8591c1f25d
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-07-09 13:29:59 -0300

    feat: solução do problema 2581 do beecrowd

---
+++ src/Iniciante/Iniciante2581.java
@@ -0,0 +1,26 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+public class Iniciante2581 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < N; i++) {
+
+            String pergunta = sc.nextLine();
+
+            System.out.println("I am Toorg!");
+
+        }
+
+        sc.close();
+
+    }
+
+}
