c2f0408db758aeee7512ffa5d930382bfb1c31bf
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-07-12 15:24:47 -0300

    feat: solução do problema 2582 do beecrowd

---
+++ src/Iniciante/iniciante2582.java
@@ -0,0 +1,94 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+public class iniciante2582 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int c = sc.nextInt();
+
+        for (int i = 0; i < c; i++) {
+
+            int x = sc.nextInt(), y = sc.nextInt();
+            int soma = x + y;
+
+            switch (soma) {
+
+                case 0:
+
+                    System.out.println("PROXYCITY");
+
+                    break;
+
+                case 1:
+
+                    System.out.println("P.Y.N.G.");
+
+                    break;
+
+                case 2:
+
+                    System.out.println("DNSUEY!");
+
+                    break;
+
+                case 3:
+
+                    System.out.println("SERVERS");
+
+                    break;
+
+                case 4:
+
+                    System.out.println("HOST!");
+
+                    break;
+
+                case 5:
+
+                    System.out.println("CRIPTONIZE");
+
+                    break;
+
+                case 6:
+
+                    System.out.println("OFFLINE DAY");
+
+                    break;
+
+                case 7:
+
+                    System.out.println("SALT");
+
+                    break;
+
+                case 8:
+
+                    System.out.println("ANSWER!");
+
+                    break;
+
+                case 9:
+
+                    System.out.println("RAR?");
+
+                    break;
+
+                case 10:
+
+                    System.out.println("WIFI ANTENNAS");
+
+                    break;
+
+            }
+
+        }
+
+        sc.close();
+
+    }
+
+}
