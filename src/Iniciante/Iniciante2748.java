a2c6b8d13a209340e0dd2ed93dac08ae5d03df6c
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-08-05 14:40:19 -0300

    feat: solução do problema 2748 do beecrowd

---
+++ src/Iniciante/Iniciante2748.java
@@ -0,0 +1,17 @@
+package Iniciante;
+
+public class Iniciante2748 {
+
+    public static void main(String[] args) {
+
+        System.out.println("---------------------------------------");
+        System.out.println("|        Roberto                      |");
+        System.out.println("|                                     |");
+        System.out.println("|        5786                         |");
+        System.out.println("|                                     |");
+        System.out.println("|        UNIFEI                       |");
+        System.out.println("---------------------------------------");
+
+    }
+
+}
