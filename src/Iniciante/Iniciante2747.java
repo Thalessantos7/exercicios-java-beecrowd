0384f093b4ccf700fee3aeb511b745418c429979
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-08-05 10:16:29 -0300

    feat: solução do problema 2747 do beecrowd

---
+++ src/Iniciante/Iniciante2747.java
@@ -0,0 +1,19 @@
+package Iniciante;
+
+public class Iniciante2747 {
+
+    public static void main(String[] args) {
+
+        System.out.println("---------------------------------------");
+
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println("|                                     |");
+
+        }
+
+        System.out.println("---------------------------------------");
+
+    }
+
+}
