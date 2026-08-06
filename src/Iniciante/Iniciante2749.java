bc7eb274a66ed074db0a7eea75ee7c2e01b92346
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-08-06 12:02:20 -0300

    feat: solução do problema 2749 do beecrowd

---
+++ src/Iniciante/Iniciante2749.java
@@ -0,0 +1,17 @@
+package Iniciante;
+
+public class Iniciante2749 {
+
+    public static void main(String[] args) {
+
+        System.out.println("---------------------------------------");
+        System.out.println("|x = 35                               |");
+        System.out.println("|                                     |");
+        System.out.println("|               x = 35                |");
+        System.out.println("|                                     |");
+        System.out.println("|                               x = 35|");
+        System.out.println("---------------------------------------");
+
+    }
+
+}
