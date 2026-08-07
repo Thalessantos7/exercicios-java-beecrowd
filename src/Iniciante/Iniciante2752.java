b64602cb7f8a43e5c5c3549050d998eb614753e2
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-08-07 18:50:48 -0300

    feat: solução do problema 2752 do beecrowd

---
+++ src/Iniciante/Iniciante2752.java
@@ -0,0 +1,18 @@
+package Iniciante;
+
+public class Iniciante2752 {
+
+    public static void main(String[] args) {
+
+        System.out.println("<AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<    AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<AMO FAZER EXERCICIO >");
+        System.out.println("<AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<AMO FAZER EXERCICIO NO URI    >");
+        System.out.println("<AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<          AMO FAZER EXERCICIO >");
+        System.out.println("<AMO FAZER EXERCICIO           >");
+
+    }
+
+}
