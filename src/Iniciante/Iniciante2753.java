3b206a8b6762ebaf7bf78a1899a1b8efda3b6280
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-08-08 12:56:41 -0300

    feat: solução do problema 2753 do beecrowd

---
+++ src/Iniciante/Iniciante2753.java
@@ -0,0 +1,15 @@
+package Iniciante;
+
+public class Iniciante2753 {
+
+    public static void main(String[] args) {
+
+        for (int i = 97; i <= 122; i++) {
+
+            System.out.println(i + " e " + (char) i);
+
+        }
+
+    }
+
+}
